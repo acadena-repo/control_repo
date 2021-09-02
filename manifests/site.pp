fc78d91370609e19e190feaac35965e348523dae
Alejandro Cadena <60622953+acadena-repo@users.noreply.github.com>
2021-09-02 10:10:08 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,4 +4,9 @@ node default {
     content => 'site pp is where puppet master first looks for configuration details about a system when the puppet agent checks in. Due to there is only one node, the puppet master, the node default declaration is used',
     owner   => 'root'
   }
+  file { '/root/NOTES':
+    ensure  => file,
+    content => 'Duplicate declarations: puppet is all about to describing your desired state so, duplicate declarations lead to an ambiguous state\n',
+               'Other built-in resources can be found at https docs.puppet.com latest type.html\n'
+  }
 }
