8dac8abec2201d192a4fb8d385e5606d4d1516e7
Alejandro Cadena <60622953+acadena-repo@users.noreply.github.com>
2021-09-02 10:12:05 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,6 @@ node default {
   }
   file { '/root/NOTES':
     ensure  => file,
-    content => 'Duplicate declarations: puppet is all about to describing your desired state so, duplicate declarations lead to an ambiguous state\n',
-               'Other built-in resources can be found at https docs.puppet.com latest type.html\n',
+    content => 'Duplicate declarations: puppet is all about to describing your desired state so, duplicate declarations lead to an ambiguous state\nOther built-in resources can be found at https docs.puppet.com latest type.html\n',
   }
 }
