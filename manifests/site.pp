ecc47999d4d7f3e9c2dea2783ec10a22e8d9ccaf
Alejandro Cadena <60622953+acadena-repo@users.noreply.github.com>
2021-09-02 09:59:35 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,5 +2,6 @@ node default {
   file { '/root/README':
     ensure  => file,
     content => 'site pp is where puppet master first looks for configuration details about a system when the puppet agent checks in. Due to there is only one node, the puppet master, the node default declaration is used',
+    owner   => 'root'
   }
 }
